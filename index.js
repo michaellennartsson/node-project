83b3683602fc3ef4c0a5204bea44ded6aae19fba
Michael Lennartsson <lennartsson_michael@hotmail.com>
2018-05-13 18:23:04 +0200

    Make server serve right assets in production #2

--- index.js
+++ index.js
@@ -23,17 +23,15 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
-/*
 if (process.env.NODE_ENV === 'production') {
   // Serve production assets like .js or .css
   app.use(express.static('client/build'));
 
   // Serve index.html file if route is not recognized
-  app.get('*', () => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-*/
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
